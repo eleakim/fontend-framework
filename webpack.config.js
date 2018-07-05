2d3226106ed5a462719694c635dbd2e8db539335
Pedro Henrique <pedro.franca@mktvirtual.com.br>
2018-07-05 16:31:39 -0300

    tentando suportar scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+
+var extend = require("xtend");
 var glob_entries = require('webpack-glob-entries');
+
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
     mode: 'development',
-    entry: glob_entries('./src/js/*.js'),
+    entry: glob_entries('./src/js/*.js', './src/scss/*.scss'),
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'public/js')
@@ -17,22 +22,30 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                    presets: ['env']
+                        presets: ['env']
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
             }
         ]
     },
     plugins: [
-        new BrowserSyncPlugin(
-            {
-                host: 'localhost',
-                port: 3000,
-                proxy: 'http://localhost/webpack'
-            },
-            {
-                reload: true
-            }
-        )
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            proxy: 'http://localhost/webpack'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
     ]
 };
